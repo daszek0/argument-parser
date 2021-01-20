a0fcb383394eed087a23abff060fa69cb987c9ca
daszek0 <daszek2003@gmail.com>
2021-01-20 01:04:13 +0100

    release

---
+++ parser.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    for (int i = 0; i < argc; i++)
+    {
+        printf("%s ", argv[i]);
+    }
+    printf("\n");
+    return 0;
+}
